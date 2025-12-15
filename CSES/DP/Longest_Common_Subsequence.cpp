67411325f4f4613e0a6417343b2e097b59cef00a
Rahul Maravi <rahulmaravi2005@gmail.com>
2025-12-15 15:23:32 +0530

    CSES

---
+++ CSES/DP/Longest_Common_Subsequence.cpp
@@ -0,0 +1,93 @@
+/**
+ *    author:  rahuliitism
+ *    created: 15.12.2025 15:03:24
+ **/
+#include <bits/stdc++.h>
+using namespace std;
+#define bset(x) __builtin_popcount(x)
+#define bsetl(x) __builtin_popcountll(x)
+#define pos(x) __builtin_ctz(x)
+#define pb push_back
+#define pob pop_back
+#define mp make_pair
+#define scan(v)       \
+    for (auto &i : v) \
+    cin >> i
+#define srt(v) sort(v.begin(), v.end())
+#define ma(v) *max_element(v.begin(), v.end())
+#define mi(v) *min_element(v.begin(), v.end())
+#define srtg(v) sort(v.begin(), v.end(), greater<>())
+#define vi(i, n) for (ll i = 0; i < n; i++)
+#define vii(i, n) for (ll i = 1; i < n; i++)
+#define viii(i, n) for (ll i = 1; i <= n; i++)
+#define all(v) v.begin(), v.end()
+#define suma(a) accumulate(all(a), 0LL)
+#define yes cout << "YES" << endl
+#define no cout << "NO" << endl
+#define lcm(a, b) (a / __gcd(a, b) * b)
+#define print(v)                      \
+    for (ll i = 0; i < v.size(); i++) \
+        cout << v[i] << ' ';          \
+    cout << endl;
+#define ff first
+#define ss second
+#define int long long
+#define sz(x) (int)(x.size())
+#define vp vector<pair<ll, ll>>
+#define endl '\n'
+using vcl = vector<long long>;
+using vcc = vector<char>;
+using ll = long long;
+#define PI 3.141592653589793238462643383279502884
+#define inf 0x3f3f3f3f
+const int MOD = 1e9 + 7;
+const int N = 1e5 + 5;
+
+void Solve()
+{
+}
+
+int32_t main()
+{
+    cin.tie(0)->sync_with_stdio(false);
+
+    int n, m;
+    cin >> n >> m;
+    vector<int> a(n);
+    scan(a);
+    vector<int> b(m);
+    scan(b);
+    vector<vector<int>> dp(n + 1, vector<int>(m + 1, 0));
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= m; j++)
+        {
+            if (a[i-1] == b[j-1])
+            {
+                dp[i][j] = 1 + dp[i-1][j-1];
+            }
+            else
+            {
+                dp[i][j] = max(dp[i-1][j],dp[i][j-1]);
+            }
+        }
+    }
+    cout << dp[n][m]<<endl;
+    int i=n;
+    int j=m;
+    vector<int>ans;
+    while(i>0 and j>0){
+        if(a[i-1]==b[j-1]){
+            ans.pb(a[i-1]);
+            i--;
+            j--;
+        }else{
+            if(dp[i-1][j]>dp[i][j-1])i--;
+            else j--;
+        }
+    }
+    reverse(all(ans));
+    print(ans);
+
+    return 0;
+}

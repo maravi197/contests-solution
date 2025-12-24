251ce80654069b07004e72589e58868289d30e65
Rahul Maravi <rahulmaravi2005@gmail.com>
2025-12-24 21:37:41 +0530

    CP

---
+++ CP/Codechef/ST218/Deleting_Array_Again.cpp
@@ -0,0 +1,83 @@
+/**
+ *    author:  rahuliitism
+ *    created: 24.12.2025 20:20:24
+ **/
+#include <bits/stdc++.h>
+using namespace std;
+#define bset(x) __builtin_popcount(x)
+#define bsetl(x) __builtin_popcountll(x)
+#define pos(x) __builtin_ctz(x)
+#define pb push_back
+#define pob pop_back
+#define mp make_pair
+#define scan(v)     \
+  for (auto &i : v) \
+  cin >> i
+#define srt(v) sort(v.begin(), v.end())
+#define ma(v) *max_element(v.begin(), v.end())
+#define mi(v) *min_element(v.begin(), v.end())
+#define srtg(v) sort(v.begin(), v.end(), greater<>())
+#define vi(i, n) for (ll i = 0; i < n; i++)
+#define vii(i, n) for (ll i = 1; i < n; i++)
+#define viii(i, n) for (ll i = 1; i <= n; i++)
+#define all(v) v.begin(), v.end()
+#define suma(a) accumulate(all(a), 0LL)
+#define yes cout << "YES" << endl
+#define no cout << "NO" << endl
+#define lcm(a, b) (a / __gcd(a, b) * b)
+#define print(v)                    \
+  for (ll i = 0; i < v.size(); i++) \
+    cout << v[i] << ' ';            \
+  cout << endl;
+#define ff first
+#define ss second
+#define int long long
+#define sz(x) (int)(x.size())
+#define vp vector<pair<ll, ll>>
+#define endl '\n'
+using vcl = vector<long long>;
+using vcc = vector<char>;
+using ll = long long;
+#define PI 3.141592653589793238462643383279502884
+#define inf 0x3f3f3f3f
+const int MOD = 1e9 + 7;
+const int NN= 1e5 + 5;
+
+void Solve()
+{ 
+    int n;cin>>n;
+    vector<int> a(n+ 1), c(n + 1);
+    for (int i = 1; i <= n ; i++) cin >> a[i];
+    for (int i = 1; i <=  n; i++) cin >> c[i];
+
+    
+    vector<vector<int>> ans(n+ 2, vector<int>(n + 2, 0));
+
+    for (int i = 1; i <= n; i++) { 
+        for (int j = 1; j <= n - i + 1; j++) {
+            int jj = j + i - 1;
+            ans[j][jj]=INT_MAX;
+            for (int k = j; k <= jj; k++) {
+            
+                int curr= ans[j][k - 1] + ans[k + 1][jj] + a[k] * c[j];
+                ans[j][jj] = min(ans[j][jj], curr);
+            }
+        }
+    }
+    cout<< ans[1][n]<<endl;
+}
+
+int32_t main()
+{
+  ios_base::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  ll _ = 1;
+  cin >> _;
+  while (_--)
+  {
+    Solve();
+  }
+
+  return 0;
+}
---
+++ CP/Codechef/ST218/Stop_The_Count.cpp
@@ -0,0 +1,75 @@
+/**
+ *    author:  rahuliitism
+ *    created: 24.12.2025 20:12:47
+ **/
+#include <bits/stdc++.h>
+using namespace std;
+#define bset(x) __builtin_popcount(x)
+#define bsetl(x) __builtin_popcountll(x)
+#define pos(x) __builtin_ctz(x)
+#define pb push_back
+#define pob pop_back
+#define mp make_pair
+#define scan(v)     \
+  for (auto &i : v) \
+  cin >> i
+#define srt(v) sort(v.begin(), v.end())
+#define ma(v) *max_element(v.begin(), v.end())
+#define mi(v) *min_element(v.begin(), v.end())
+#define srtg(v) sort(v.begin(), v.end(), greater<>())
+#define vi(i, n) for (ll i = 0; i < n; i++)
+#define vii(i, n) for (ll i = 1; i < n; i++)
+#define viii(i, n) for (ll i = 1; i <= n; i++)
+#define all(v) v.begin(), v.end()
+#define suma(a) accumulate(all(a), 0LL)
+#define yes cout << "YES" << endl
+#define no cout << "NO" << endl
+#define lcm(a, b) (a / __gcd(a, b) * b)
+#define print(v)                    \
+  for (ll i = 0; i < v.size(); i++) \
+    cout << v[i] << ' ';            \
+  cout << endl;
+#define ff first
+#define ss second
+#define int long long
+#define sz(x) (int)(x.size())
+#define vp vector<pair<ll, ll>>
+#define endl '\n'
+using vcl = vector<long long>;
+using vcc = vector<char>;
+using ll = long long;
+#define PI 3.141592653589793238462643383279502884
+#define inf 0x3f3f3f3f
+const int MOD = 1e9 + 7;
+const int N = 1e5 + 5;
+
+void Solve()
+{ 
+  int n;cin>>n;
+  string s;cin>>s;
+  int ans=0;
+  int cnt=0;
+  int cntt=0;
+  vi(i,n){
+    if(s[i]=='1')cnt++;
+    else cntt++;
+    if(cnt>cntt)ans++;
+  }
+  cout<<ans<<endl;
+    
+}
+
+int32_t main()
+{
+  ios_base::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  ll _ = 1;
+  cin >> _;
+  while (_--)
+  {
+    Solve();
+  }
+
+  return 0;
+}
